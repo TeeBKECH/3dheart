306cec5803be151a0426e2af83f2fbfdc1d14c41
TeeBKECH <t.e.r.r.a.nik566@gmail.com>
2022-01-28 15:47:20 +0300

    new heart.gltf

--- src/Canvas/Components/Heart.jsx
+++ src/Canvas/Components/Heart.jsx
@@ -15,8 +15,8 @@ const Heart = (props) => {
       object={theModel}
       scale={[1, 1, 1]}
       rotation={[0, 0, 0]}
-      receiveShadow={true}
-      castShadow={true}
+      receiveShadow
+      castShadow
     >
     </primitive>
   );
