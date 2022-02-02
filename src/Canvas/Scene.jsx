29fdcded4a298839cee933f3fcb9ba613871b512
TeeBKECH <t.e.r.r.a.nik566@gmail.com>
2022-02-02 13:36:06 +0300

    add react-three-drei

--- src/Canvas/Scene.jsx
+++ src/Canvas/Scene.jsx
@@ -1,6 +1,7 @@
 import { Suspense, useEffect } from "react";
 import * as THREE from "three";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Environment } from '@react-three/drei';
 import { extend, useThree } from "react-three-fiber";
 import Floor from "./Components/Floor";
 import Heart from "./Components/Heart";
@@ -17,18 +18,18 @@ const Scene = ({newMaterialOpt}) => {
 
   useEffect(() => {
 
-    const bgLoader = new THREE.CubeTextureLoader();
-    bgLoader.setPath( 'bg/' );
-    console.log(bgLoader);
-    let textureCube = bgLoader.load( [ 'px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg' ] );
-    textureCube.encoding = THREE.sRGBEncoding;
-    scene.background = textureCube;
+    // const bgLoader = new THREE.CubeTextureLoader();
+    // bgLoader.setPath( 'bg/' );
+    // console.log(bgLoader);
+    // let textureCube = bgLoader.load( [ 'px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg' ] );
+    // textureCube.encoding = THREE.sRGBEncoding;
+    // scene.background = textureCube;
 
     const directionalLight = scene.children[1];
     // scene.background = new THREE.Color(0x7de1ff);
     // scene.fog = new THREE.Fog(0x7de1ff, 20, 100);
     directionalLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
-    shadowMap.enabled = true;
+    // shadowMap.enabled = true;
     // console.log(scene);
   }, []);
 
@@ -50,6 +51,13 @@ const Scene = ({newMaterialOpt}) => {
       <Suspense fallback={null}>
         <Heart newMaterialOpt={newMaterialOpt} />
         {/* <Floor /> */}
+        <Environment
+          background // Whether to affect scene.background
+          files={'ballroom_4k.hdr'} // Array of cubemap files OR single equirectangular file
+          path={'/bg/hdr/'} // Path to the above file(s)
+          preset={null} // Preset string (overrides files and path)
+          scene={scene} // adds the ability to pass a custom THREE.Scene
+        />
       </Suspense>
     </>
   );
